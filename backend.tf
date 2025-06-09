e66ede306d29f5772c53430676984d75e9a46f48
arpolek <38766329+arpolek@users.noreply.github.com>
2025-06-09 13:47:46 +0200

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    endpoint                    = "it-mil-1.linodeobjects.com"
+    bucket                      = "tf-state"
+    key                         = "terraform.tfstate"
+    region                      = "eu-south-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
