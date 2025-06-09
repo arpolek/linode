5003ccadcfeb910b3e1a63a5d711cb3c743e6b78
arpolek <38766329+arpolek@users.noreply.github.com>
2025-06-09 13:52:23 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     bucket                      = "tf-state"
     key                         = "terraform.tfstate"
     region                      = "eu-south-1"
+    skip_requesting_account_id = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
