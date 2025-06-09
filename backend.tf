fdb4f9ded472b2a03ffa0207d027cf900077d363
arpolek <38766329+arpolek@users.noreply.github.com>
2025-06-09 16:04:43 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -8,7 +8,5 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
-    skip_s3_checksum            = true
-    skip_region_validation      = true
   }
 }
